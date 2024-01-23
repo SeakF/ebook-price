e3eb400207bb121e1835e787a401e5efe1af8f45
unknown <fgozdz01@gmail.com>
2024-01-23 14:34:35 +0100

    delete boilerplate files

--- src/http-extension/http-extension.service.spec.ts
+++ src/http-extension/http-extension.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HttpExtensionService } from './http-extension.service';
+import { HttpModule } from '@nestjs/axios';
 
 describe('HttpExtensionService', () => {
   let service: HttpExtensionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule.register({})],
       providers: [HttpExtensionService],
     }).compile();
 
